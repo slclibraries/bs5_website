9bceb998207cbfeae559a641f2dd968a9db060b4
Katherine McMillan <131383792+slclibraries@users.noreply.github.com>
2023-11-07 13:53:05 -0500

    Create look_feel_js.js

---
+++ look_feel_js.js
@@ -0,0 +1,19 @@
+<script>
+  (function() {
+    var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?719";
+    var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
+  })();
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("lib-chat-button").style.bottom = "0";
+    document.getElementById("pop").style.top = "0";
+  } else {
+    document.getElementById("lib-chat-button").style.bottom = "-50px";
+    document.getElementById("pop").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+</script>
