db192978ea41f7a6b6c35dbfad19bff63ae007af
Katherine McMillan <131383792+slclibraries@users.noreply.github.com>
2024-09-04 08:44:53 -0400

    Update look_feel.js

--- look_feel.js
+++ look_feel.js
@@ -1,19 +1,15 @@
+<script type="text/javascript">
+function searchPrimo() {
+document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value;
+document.forms["searchForm"].submit();
+}
+</script>
+
+<!-- Chat Service DO NOT REMOVE -->
 <script>
   (function() {
     var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
     x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?719";
     var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
   })();
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("lib-chat-button").style.bottom = "0";
-    document.getElementById("pop").style.top = "0";
-  } else {
-    document.getElementById("lib-chat-button").style.bottom = "-50px";
-    document.getElementById("pop").style.top = "-143px";
-  }
-  prevScrollpos = currentScrollPos;
-}
 </script>
